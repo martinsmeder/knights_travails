676eb71ed7f7b3ee6542ebe96ef808a9ccfbb41c
Martin Smeder <martinsmeder91@gmail.com>
2024-02-13 20:54:14 +0100

    Added breadthFirstTraversal()

--- src/javascript/logic.js
+++ src/javascript/logic.js
@@ -1,4 +1,54 @@
-function GraphFactory() {
+function QueueFactory() {
+  const queue = [];
+  let head = 0;
+  let tail = 0;
+  const maxSize = 100;
+
+  function enqueue(item) {
+    if (isFull()) {
+      return false;
+    }
+    queue[tail] = item;
+    tail++;
+    return true;
+  }
+
+  function dequeue() {
+    if (isEmpty()) {
+      return null;
+    }
+    const item = queue[head];
+    head++;
+    return item;
+  }
+
+  function peek() {
+    return isEmpty() ? null : queue[head];
+  }
+
+  function getLength() {
+    return tail - head;
+  }
+
+  function isEmpty() {
+    return getLength() === 0;
+  }
+
+  function isFull() {
+    return getLength() === maxSize;
+  }
+
+  return {
+    enqueue,
+    dequeue,
+    peek,
+    getLength,
+    isEmpty,
+    isFull,
+  };
+}
+
+export function GraphFactory() {
   const adjacencyList = {};
 
   function addVertex(vertex) {
@@ -59,19 +109,33 @@ function GraphFactory() {
     }
   }
 
-  function placeKnight(location) {
-    const neighbors = adjacencyList[location].slice(); // Deep copy
-    const knight = KnightFactory(location); // Create knight object
+  //   function placeKnight(location) {
+  //     const neighbors = adjacencyList[location].slice(); // Deep copy
+  //     const knight = KnightFactory(location); // Create knight object
 
-    removeVertex(location); // Remove vertex at chosen location
-    addVertex(knight); // Add knight location string as replacement
+  //     removeVertex(location); // Remove vertex at chosen location
+  //     addVertex(knight); // Add knight location string as replacement
 
-    // Loop through list of neighbors
-    neighbors.forEach((neighbor) => {
-      // Add edge between knight and neighbor
-      addEdge(knight, neighbor);
-    });
-  }
+  //     // Loop through list of neighbors
+  //     neighbors.forEach((neighbor) => {
+  //       // Add edge between knight and neighbor
+  //       addEdge(knight, neighbor);
+  //     });
+  //   }
+
+  //   function setDestination(location) {
+  //     const neighbors = adjacencyList[location].slice(); // Deep copy
+  //     const destination = DestinationFactory(location); // Create destination object
+
+  //     removeVertex(location); // Remove vertex at chosen location
+  //     addVertex(destination); // Add destination location string as replacement
+
+  //     // Loop through list of neighbors
+  //     neighbors.forEach((neighbor) => {
+  //       // Add edge between destination and neighbor
+  //       addEdge(destination, neighbor);
+  //     });
+  //   }
 
   return {
     adjacencyList,
@@ -81,31 +145,50 @@ function GraphFactory() {
     removeEdge,
     removeVertex,
     createGrid,
-    placeKnight,
+    // placeKnight,
+    // setDestination,
   };
 }
 
-function KnightFactory(location) {
-  function getLocation() {
-    return `Knight at: ${location}`;
-  }
+// function KnightFactory(location) {
+//   function getLocation() {
+//     return `Knight at: ${location}`;
+//   }
 
-  return getLocation();
-}
+//   return getLocation();
+// }
+
+// function DestinationFactory(location) {
+//   function getLocation() {
+//     return `Destination at: ${location}`;
+//   }
+
+//   return getLocation();
+// }
 
 const graph = GraphFactory();
-graph.createGrid();
-graph.printAdjacencyList();
-graph.placeKnight("A1");
-graph.printAdjacencyList();
 
-// 6. Set destination (ending square)
-// -->
-// -->
-// -->
-// -->
-// -->
-// -->
-// 7. Make move
-// 8. Use chosen search algorithm to find shortest path between starting square
-//    and ending square
+function breadthFirstTraversal(graph, startingVertex) {
+  const visited = new Set();
+  const result = [];
+  const queue = QueueFactory();
+
+  queue.enqueue(startingVertex);
+  visited.add(startingVertex);
+
+  while (!queue.isEmpty()) {
+    const currentVertex = queue.dequeue();
+    result.push(currentVertex);
+
+    for (const neighbor of graph.adjacencyList[currentVertex]) {
+      if (!visited.has(neighbor)) {
+        queue.enqueue(neighbor);
+        visited.add(neighbor);
+      }
+    }
+  }
+
+  return result;
+}
+
+breadthFirstTraversal(graph, "A1");
